a061ed45dcb27f11a1e42a5b48fcb89dc9218cbc
Benedicte <101096042+bendiz@users.noreply.github.com>
2024-04-07 01:31:34 +0200

    use react-bootstrap

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,37 +1,35 @@
 import { BiMath } from 'react-icons/bi';
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+import { scrollToTop } from '../utilities/utils';
+import { useEffect, useState } from 'react';
 
-export default function Navbar(props: any) {
+export default function NavigationBar(props: any) {
   return (
-    <nav className='navbar navbar-expand-lg bg-body-tertiary mb-3'>
-      <div className='container-fluid'>
-        <span className='navbar-brand d-flex'>
+    <Navbar className='fixed-top' expand='lg' bg='light' variant='light'>
+      <Container>
+        <Navbar.Brand>
           <BiMath />
-        </span>
-        <button
-          className='navbar-toggler'
-          type='button'
-          data-bs-toggle='collapse'
-          data-bs-target='#navbarNav'
-          aria-controls='navbarNav'
-          aria-expanded='false'
-          aria-label='Toggle navigation'>
-          <span className='navbar-toggler-icon'></span>
-        </button>
-        <div className='collapse navbar-collapse' id='navbarNav'>
-          <ul className='navbar-nav'>
-            <li className='nav-item'>
-              <a className='nav-link active' aria-current='page' onClick={props.changeQuery}>
-                Ny spørring
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link active' aria-current='page' onClick={props.grafVisning}>
-                {props.grafVisningOn ? 'Tabellvisning' : 'Grafvisning'}
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='me-auto'>
+            <Nav.Link onClick={props.changeQuery}>Ny spørring</Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                props.grafVisning();
+                scrollToTop();
+              }}>
+              {props.grafVisningOn ? 'Tabellvisning' : 'Grafvisning'}
+            </Nav.Link>
+            <NavDropdown title='Statistikkgrafer' id='basic-nav-dropdown' onClick={props.handleFirstClick}>
+              <NavDropdown.Item onClick={() => props.refs.scrollToRef(props.refs.medianRef)}>Median</NavDropdown.Item>
+              <NavDropdown.Item onClick={() => props.refs.scrollToRef(props.refs.avgRef)}>Gjennomsnitt</NavDropdown.Item>
+              <NavDropdown.Item onClick={() => props.refs.scrollToRef(props.refs.minRef)}>Minimum</NavDropdown.Item>
+              <NavDropdown.Item onClick={() => props.refs.scrollToRef(props.refs.maxRef)}>Maksimum</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
