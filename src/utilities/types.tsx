b22122614c79c8b9d24270b061d3b2e7e07b2bb5
Benedicte <101096042+bendiz@users.noreply.github.com>
2024-04-07 01:30:24 +0200

    refactoring

--- src/utilities/types.tsx
+++ src/utilities/types.tsx
@@ -8,4 +8,12 @@ export type RegionType = {
   navn: string;
 };
 
+export type TableDataType = {
+  indexes: any[];
+  variable: any[];
+  region: any[];
+  year: any[];
+  values: any[];
+};
+
 export type LabelKey = 'median' | 'avg' | 'min' | 'max';
--- src/utilities/utils.tsx
+++ src/utilities/utils.tsx
@@ -1,3 +1,14 @@
+import { TableDataType } from './types';
+
+export function processData(data: any): TableDataType {
+  return {
+    indexes: Object.values(data.dimension.Region.category.index),
+    variable: Object.values(data.dimension.ContentsCode.category.label),
+    region: Object.values(data.dimension.Region.category.label),
+    year: Object.values(data.dimension.Tid.category.label),
+    values: data.value,
+  };
+}
 export function errorCheck(selectedOptions: any): [boolean, string[]] {
   let errors = [''];
   let errorFound = false;
@@ -14,6 +25,26 @@ export function errorCheck(selectedOptions: any): [boolean, string[]] {
     errorFound = true;
   }
   return [errorFound, errors];
+}
+
+export function scrollToRef(ref: any, yOffset = -100) {
+  if (ref && ref.current) {
+    const y = ref.current.getBoundingClientRect().top + window.pageYOffset + yOffset;
+    window.scrollTo({ top: y, behavior: 'smooth' });
+  }
+}
+
+export function scrollToTop() {
+  return window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+export function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 export function calculateMedian(values: number[]): number {
